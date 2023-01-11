8b811b443bb331df01fc78955818bb79bf8b43f4
YAKSHIT-22 <yakshitgarg25@gmail.com>
2023-01-11 16:33:12 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgetPassword from './pages/ForgetPassword';
+
 
 
 function App() {
   return (
-    <div className="bg-red-100">
-      <h1 className="text-7xl">hello world</h1>
-    </div>
+    <>
+    <Router>
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/offers' element={<Offers/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/sign-in' element={<SignIn/>}/>
+          <Route path='/sign-up' element={<SignUp/>}/>
+          <Route path='/forget-password' element={<ForgetPassword/>}/>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
---
+++ src/pages/ForgetPassword.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ForgetPassword() {
+  return (
+    <div>ForgetPassword</div>
+  )
+}
